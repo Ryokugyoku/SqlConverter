3345c3685682f6b956f4a01ffa54d9243f28e20c
maeda <mashumaro1997@icloud.com>
2024-06-22 22:41:34 +0900

    メインページの文字変更

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,8 @@ function App() {
         <Menu />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+           My  Wiki Project @2024 MM
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
