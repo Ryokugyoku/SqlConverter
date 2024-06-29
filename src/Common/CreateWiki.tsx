9ddf58b14d6b7dce717bb2b82174e6c9996ff681
maeda <mashumaro1997@icloud.com>
2024-06-29 10:20:37 +0900

    ローカルストレージを利用したデータ保存

--- src/Common/CreateWiki.tsx
+++ src/Common/CreateWiki.tsx
@@ -1,40 +1,56 @@
 import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import './CreateWiki.css';
 interface Props {
     // Define your component's props here
 }
 
 const CreateWiki: React.FC<Props> = () => {
-    const [date, setDate] = useState(''); // 日付の状態
+    const [date] = useState(new Date().toISOString()); // 日付の状態
     const [text, setText] = useState(''); // テキストボックスの値の状態
-
-    const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setDate(e.target.value);
-    };
+    const [title, setTitle] = useState(''); // テキストボックスの値の状態
 
     const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setText(e.target.value);
     };
 
+    const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value);
+    };
+
     const saveData = () => {
-        const dataToSave = { date, text };
+        const dataToSave = { date, text, title};
         localStorage.setItem('myData', JSON.stringify(dataToSave));
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            e.preventDefault(); // Enterのデフォルトの動作を防ぐ
+            const formElements = Array.from(document.querySelectorAll('input, textarea'));
+            const currentIndex = formElements.indexOf(document.activeElement as Element);
+            const nextElement = formElements[currentIndex + 1];
+            if (nextElement) {
+                (nextElement as HTMLElement).focus(); // 次の要素にフォーカスを移動
+            }
+        }
     };
 
     return (
         <div className='CreateWiki'>
-            <input type="date" value={date} onChange={handleDateChange} />
             <div className='Editor'>
-                <div className="toggle-button-cover">
-                    <div id="button-3" className="button r">
-                    <input className="checkbox" type="checkbox"/>
-                    <div className="knobs"></div>
-                    <div className="layer"></div>
+                <div className=''>Add Note</div>
+                <input value={title} onChange={handleTitleChange} onKeyDown={handleKeyDown} className='TitleArea' placeholder='Title'/>
+                <div className='InputBox'>
+                    <textarea value={text} onChange={handleTextChange} className='TextInputArea'/>
+                    <div className="markdown-preview">
+                        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                            {text}
+                        </ReactMarkdown>
                     </div>
                 </div>
-                <textarea value={text} onChange={handleTextChange} className='TextInputArea'/>
             </div>
-            <button onClick={saveData}>Save Data</button>
+            <button onClick={saveData} className='SaveButton'><span>Save Data</span></button>
         </div>
     );
 };
